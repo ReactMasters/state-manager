7013623b2d05d1063871f1bf96c239c196b04515
jordan-choi <yyoungbest@gmail.com>
2024-07-27 12:06:15 +0100

    feat: add selector

--- src/store.test.ts
+++ src/store.test.ts
@@ -4,12 +4,16 @@ describe("Store class", () => {
   describe("subscribe method", () => {
     it("should add a listener to the listeners array", () => {
       const store = new Store(0);
-      const listener = vitest.fn() as Listener; // Mock listener function
+      const listener = vitest.fn() as Listener<number>; // Mock listener function
 
-      const unsubscribe = store.subscribe(listener);
+      const unsubscribe = store.subscribe((v) => v, listener);
 
       expect(store.listeners.size).toBe(1);
-      expect(store.listeners.has(listener)).toBeTruthy();
+      expect(
+        Array.from(store.listeners.values()).some(
+          ({ listener: l }) => listener === l
+        )
+      ).toBeTruthy();
 
       unsubscribe(); // Call unsubscribe to avoid memory leaks
     });
@@ -18,47 +22,61 @@ describe("Store class", () => {
   describe("unsubscribe method", () => {
     it("should remove a listener from the listeners array", () => {
       const store = new Store(0);
-      const listener1 = vitest.fn() as Listener;
-      const listener2 = vitest.fn() as Listener;
+      const listener1 = vitest.fn() as Listener<number>;
+      const listener2 = vitest.fn() as Listener<number>;
+      const selector = (v: number) => v;
 
-      store.subscribe(listener1);
-      store.subscribe(listener2);
+      const unsubscribe1 = store.subscribe(selector, listener1);
+      store.subscribe(selector, listener2);
 
       expect(store.listeners.size).toBe(2);
 
-      store.unsubscribe(listener1);
+      unsubscribe1();
 
       expect(store.listeners.size).toBe(1);
-      expect(store.listeners.has(listener1)).toBeFalsy();
-      expect(store.listeners.has(listener2)).toBeTruthy();
+
+      expect(
+        Array.from(store.listeners.values()).some(
+          ({ listener: l }) => listener1 === l
+        )
+      ).toBeFalsy();
+      expect(
+        Array.from(store.listeners.values()).some(
+          ({ listener: l }) => listener2 === l
+        )
+      ).toBeTruthy();
     });
 
     it("should remove a listner by clean-up function", () => {
       const store = new Store(0);
-      const listener1 = vitest.fn() as Listener;
-      const listener2 = vitest.fn() as Listener;
+      const listener1 = vitest.fn() as Listener<number>;
+      const listener2 = vitest.fn() as Listener<number>;
 
-      const unsub = store.subscribe(listener1);
-      store.subscribe(listener2);
+      const unsub = store.subscribe((v) => v, listener1);
+      store.subscribe((v) => v, listener2);
 
       expect(store.listeners.size).toBe(2);
 
       unsub();
 
       expect(store.listeners.size).toBe(1);
-      expect(store.listeners.has(listener1)).toBeFalsy();
+      expect(
+        Array.from(store.listeners.values()).some(
+          ({ listener: l }) => listener1 === l
+        )
+      ).toBeFalsy();
     });
 
     it("should not modify the array if listener is not found", () => {
       const store = new Store(0);
-      const listener1 = vitest.fn() as Listener;
-      const listener2 = vitest.fn() as Listener;
+      const listener1 = vitest.fn() as Listener<number>;
 
-      store.subscribe(listener1);
+      store.subscribe((v) => v, listener1);
 
       const originalSize = store.listeners.size;
 
-      store.unsubscribe(listener2);
+      const nonExistentKey = Symbol();
+      store.unsubscribe(nonExistentKey);
 
       expect(store.listeners.size).toBe(originalSize);
     });
@@ -67,9 +85,9 @@ describe("Store class", () => {
   describe("setState method", () => {
     it("should update the internal state and call listeners", () => {
       const store = new Store(0);
-      const listener = vitest.fn() as Listener;
+      const listener = vitest.fn() as Listener<number>;
 
-      store.subscribe(listener);
+      store.subscribe((v) => v, listener);
 
       const nextState = 1;
       store.setState(nextState);
@@ -81,9 +99,9 @@ describe("Store class", () => {
     it("should not call listener when the same value is passed", () => {
       const unchangedState = {};
       const store = new Store(unchangedState);
-      const listener = vitest.fn() as Listener;
+      const listener = vitest.fn() as Listener<typeof unchangedState>;
 
-      store.subscribe(listener);
+      store.subscribe((v) => v, listener);
 
       store.setState(unchangedState);
 
@@ -109,11 +127,11 @@ describe("Store class", () => {
   describe("broadcast method", () => {
     it("should call all subscribed listeners with the current state", () => {
       const store = new Store(0);
-      const listener1 = vitest.fn() as Listener;
-      const listener2 = vitest.fn() as Listener;
+      const listener1 = vitest.fn() as Listener<number>;
+      const listener2 = vitest.fn() as Listener<number>;
 
-      store.subscribe(listener1);
-      store.subscribe(listener2);
+      store.subscribe((v) => v, listener1);
+      store.subscribe((v) => v, listener2);
 
       store.broadcast();
 
--- src/store.ts
+++ src/store.ts
@@ -1,34 +1,42 @@
-export type Listener = () => void;
+export type Listener<T> = (state: T) => void;
+type Selector<T, U> = (state: T) => U;
+type Updater<T> = (state: T) => T;
 
-export class Store<T> {
-  listeners: Set<Listener> = new Set();
+export class Store<T extends U, U = T> {
+  listeners: Map<symbol, { selector: Selector<T, U>; listener: Listener<U> }> =
+    new Map();
   state: T;
 
   constructor(initialState: T) {
     this.state = initialState;
   }
 
-  subscribe(listener: Listener) {
-    // listener 배열에 삽입
-    this.listeners.add(listener);
+  subscribe(selector: Selector<T, U>, listener: Listener<U>) {
+    const key = Symbol();
+
+    this.listeners.set(key, {
+      selector,
+      listener,
+    });
 
     // cleanup
-    return () => this.unsubscribe(listener);
+    return () => this.unsubscribe(key);
   }
 
-  unsubscribe(listener: Listener) {
-    this.listeners.delete(listener);
+  unsubscribe(key: symbol) {
+    if (!this.listeners.has(key)) {
+      return;
+    }
+
+    this.listeners.delete(key);
   }
 
   getState(): T {
     return this.state;
   }
 
-  setState(newState: T | ((currentState: T) => T)) {
-    const nextState =
-      typeof newState === "function"
-        ? (newState as (currentState: T) => T)(this.state)
-        : newState;
+  setState(newState: U | Updater<T>) {
+    const nextState = this.getNextState(this.state, newState);
 
     const shouldUpdate = nextState !== this.state;
     if (!shouldUpdate) return this.state;
@@ -38,7 +46,25 @@ export class Store<T> {
     this.broadcast();
   }
 
+  private getNextState(currentState: T, newState: U | Updater<T>): T {
+    if (typeof newState === "function") {
+      return (newState as Updater<T>)(currentState);
+    }
+
+    const isNonNullObject = (value: unknown): value is object =>
+      typeof value === "object" && value !== null;
+
+    if (isNonNullObject(currentState) && isNonNullObject(newState)) {
+      return { ...currentState, ...newState } as T;
+    }
+
+    return newState as T;
+  }
+
   broadcast() {
-    this.listeners.forEach((l) => l());
+    for (const { selector, listener } of this.listeners.values()) {
+      const selectedState = selector(this.state);
+      listener(selectedState);
+    }
   }
 }
