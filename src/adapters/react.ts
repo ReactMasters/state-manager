9d485e85837468e2a0070e2c209177e79d1e3c0b
JeGwan <emperorvv@gmail.com>
2024-08-11 19:58:24 +0900

    Add comment about cleanup

--- src/adapters/react.ts
+++ src/adapters/react.ts
@@ -21,7 +21,7 @@ export function useReactive<T, O = T>(
   selector: Selector<T, O> = (count) => count as unknown as O
 ): O {
   return useSyncExternalStore<O>(
-    // subscribe: 상태 변경되면 리렌더링을 일으킬 listener 를 받아서 핸들링
+    // subscribe: 상태 변경되면 리렌더링을 일으킬 listener 를 받아서 핸들링, cleanup 을 반환해야함
     (listener) => stateManager.store.subscribe(listener, selector),
     // getSnapshot: 현재 상태를 반환
     () => selector(stateManager.get()),
