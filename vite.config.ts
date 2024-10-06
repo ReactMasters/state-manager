5967843ff9940ff648deda6e8a64e8cc2afdbdf5
JeGwan <emperorvv@gmail.com>
2024-10-06 22:18:04 +0900

    wrench: Add type declaration file config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      outDir: 'dist',
+      entryRoot: 'src',
+      exclude: ['**/*.test.ts', '**/*.test.tsx'], // 테스트 파일 제외
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
